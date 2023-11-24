efc03d2b2ae654e482ccaa3a8b3a7e9502c62300
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-11-23 22:14:38 -0500

    added new project

--- js/header.js
+++ js/header.js
@@ -11,14 +11,18 @@ class ProjectHeader extends HTMLElement {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a class="nav-link ${isActive('/project_budget.html')}" href="./project_budget.html">Budget Tracking</a>
+                                <a class="nav-link ${isActive('/project_c2.html')}" href="./project_c2.html">Command & Control</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link ${isActive('/project_SMTP.html')}" href="./project_SMTP.html">SMTP Form</a>
+                                <a class="nav-link ${isActive('/project_budget.html')}" href="./project_budget.html">Budget Tracking</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link ${isActive('/project_phishing.html')}" href="./project_phishing.html">Phshing</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link ${isActive('/project_SMTP.html')}" href="./project_SMTP.html">SMTP Form</a>
+                            </li>
+       
                         </ul>
                     </div>
                 </div>
